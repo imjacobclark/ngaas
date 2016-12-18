12691edf63d2e0437e381414013f8d562b9f832c
Jacob Clark <jacob.jh.clark@googlemail.com>
2016-12-18 14:15:59 +0000

    Basic analytics

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express'),
     app = express(),
-    generator = require('./generator').generate;
+    generator = require('./generator').generate,
+    requests = 0;
 
 app.get('/', function (req, res) {
+    requests++;
+    
     res.header('Access-Control-Allow-Origin', '*');
 
     res.json({
         name: generator()
     });
+    
+    console.log(requests);
 })
 
 var server = app.listen(3000);
